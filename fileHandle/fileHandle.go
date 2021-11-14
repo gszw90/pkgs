91c925dc4dd885ed4269838e0d0499358e683121
gszw90 <13350016962@163.com>
2021-11-14 16:24:59 +0800

    feat: 添加时间锁与文件操作

---
+++ fileHandle/fileHandle.go
@@ -0,0 +1,41 @@
+package fileHandle
+
+import "os"
+
+// IsFileExisted 文件或者目录是否存在
+func IsFileExisted(filePath string) (ok bool, err error) {
+	_, err = os.Stat(filePath)
+	if err == nil {
+		ok = true
+		return
+	}
+	if os.IsNotExist(err) {
+		err = nil
+	}
+	return
+}
+
+// CreateDir 创建一级或者多级目录
+func CreateDir(dirPath string) (err error) {
+	ok, err := IsFileExisted(dirPath)
+	if ok {
+		return err
+	}
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(dirPath, os.ModePerm)
+	return err
+}
+
+// IsDir 判断是否为目录
+func IsDir(dirPath string) (ok bool) {
+	fi, err := os.Stat(dirPath)
+	if err != nil {
+		return
+	}
+	if fi.IsDir() {
+		ok = true
+	}
+	return
+}
---
+++ timeLock/timeLock.go
@@ -0,0 +1,56 @@
+package timeLock
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+const (
+	_stateUnlock        int32 = 0 // 未上锁状态
+	_stateLocked        int32 = 1 // 普通锁状态
+	_stateReleaseLocked int32 = 3 // 锁有释放时间的上锁状态
+)
+
+type TimeLock struct {
+	state int32
+}
+
+// TryLockWithTimeout 在一段时间内持续尝试获取锁
+func (l *TimeLock) TryLockWithTimeout(t time.Duration) (ok bool) {
+	mils := t.Milliseconds()
+	for mils > 0 {
+		if atomic.CompareAndSwapInt32(&(l.state), _stateUnlock, _stateLocked) {
+			ok = true
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+		mils -= 100
+	}
+	return
+}
+
+// LockWithReleaseTime 获取锁,如果锁的持续时间超过t,则自动释放锁
+func (l *TimeLock) LockWithReleaseTime(t time.Duration) {
+	for {
+		if atomic.CompareAndSwapInt32(&(l.state), _stateUnlock, _stateReleaseLocked) {
+			go func() {
+				<-time.After(t)
+				l.Unlock(true)
+			}()
+			break
+		}
+	}
+}
+
+// Unlock 释放锁
+func (l *TimeLock) Unlock(isReleasedLock bool) {
+	if isReleasedLock {
+		atomic.CompareAndSwapInt32(&(l.state), _stateReleaseLocked, _stateUnlock)
+	} else {
+		atomic.CompareAndSwapInt32(&(l.state), _stateLocked, _stateUnlock)
+	}
+}
+
+func NewTimeLock() *TimeLock {
+	return &TimeLock{state: _stateUnlock}
+}
