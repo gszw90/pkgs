a817dd585342280e032868627b4878f4586e2dd5
gszw90 <13350016962@163.com>
2021-11-14 16:40:24 +0800

    feat: message

---
+++ message/message.go
@@ -0,0 +1,51 @@
+package message
+
+const (
+	CodeSuccess  = 200
+	CodeFail     = 400
+	CodeNotFound = 404
+
+	MsgSuccess  = "success"
+	MsgFail     = "fail"
+	MsgNotFound = "not found"
+)
+
+// Message 格式化返回消息
+type Message struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+// NewMessage return message
+func NewMessage(code int, msg string, data interface{}) Message {
+	return Message{
+		Code: code,
+		Msg:  msg,
+		Data: data,
+	}
+}
+
+// SuccessMessage success
+func SuccessMessage(data interface{}, msg string) Message {
+	if msg == "" {
+		msg = MsgSuccess
+	}
+	return NewMessage(CodeSuccess, msg, data)
+}
+
+// FailMessage fail
+func FailMessage(msg string, data interface{}) Message {
+	if msg == "" {
+		msg = MsgFail
+	}
+	return NewMessage(CodeFail, msg, data)
+}
+
+// NotFoundMessage not found
+func NotFoundMessage(msg string, data interface{}) Message {
+	if msg == "" {
+		msg = MsgNotFound
+	}
+	return NewMessage(CodeNotFound, msg, data)
+}
